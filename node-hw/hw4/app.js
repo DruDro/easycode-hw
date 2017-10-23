597c8dc933a9a2c0851fcfa0f61b24e8d5a2bc84
Dru Dro <drudro@outlook.com>
2017-10-23 15:08:56 +0300

    async added

--- node-hw/hw4/app.js
+++ node-hw/hw4/app.js
@@ -2,5 +2,8 @@ const fs = require('file-system');
 const dirname = 'date';
 let date = (new Date()).toLocaleString();
 
-fs.mkdir(dirname);
-fs.writeFile(`${dirname}/date.txt`, date);
+(async function createDir() {
+	await fs.mkdir(dirname);
+	fs.writeFile(`${dirname}/date.txt`, date);
+}());
+
