0b57212d451a805d48ef468d8e819ec39ca7492b
Dru Dro <drudro@outlook.com>
2017-10-23 16:38:32 +0300

    try...catch

--- node-hw/hw4/app.js
+++ node-hw/hw4/app.js
@@ -2,14 +2,10 @@ const fs = require('file-system');
 const dirname = 'date';
 let date = (new Date()).toLocaleString();
 
-const prom = new Promise((resolve, reject) => {
-    try {
-        fs.mkdirSync(dirname);
-    } catch (e) {
-        reject(e => e);
-    }
-    resolve();
 
-});
-prom.then(() => fs.writeFile(`${dirname}/date.txt`, date))
-prom.catch(e => console.log(e));
+try {
+    fs.mkdirSync(dirname);
+    fs.writeFile(`${dirname}/date.txt`, date)
+} catch (e) {
+    console.log(e)
+}
